a64d297950175d704908b4d29ab1c4dbed1605b6
JasmineRain <530781348@qq.com>
2020-10-31 17:32:37 +0800

    2020.10.31 GO LPL GO

--- 30_Hard_串联所有单词的字串.py
+++ 30_Hard_串联所有单词的字串.py
@@ -2,9 +2,36 @@ from typing import List
 
 
 class Solution:
-    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+    def findSubstring_Iterate(self, s: str, words: List[str]) -> List[int]:
+        words_map = {}
+        for word in words:
+            words_map.setdefault(word, 0)
+            words_map[word] += 1
+        ans = []
+        length = len(s)
+        word_len = len(words[0])
+        word_num = len(words)
+        for i in range(0, length - word_len * word_num + 1):
+
+            cur_words = {}
+            cur_word_num = 0
+            substr = s[i: i + word_len * word_num]
+
+            while cur_word_num < word_num:
+                cur_word = substr[cur_word_num * word_len: (cur_word_num + 1) * word_len]
+                if cur_word in words_map.keys():
+                    cur_words.setdefault(cur_word, 0)
+                    cur_words[cur_word] += 1
+                    if cur_words[cur_word] > words_map[cur_word]:
+                        break
+                else:
+                    break
+                cur_word_num += 1
+            if cur_word_num == word_num:
+                ans.append(i)
+        return ans
 
 
 if __name__ == "__main__":
     S = Solution()
-    print(S.findSubstring())
+    print(S.findSubstring_Iterate(s="barfoothefoobarman", words=["foo", "bar"]))
