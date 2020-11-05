51692a895e88d6b139088c2dfb171134c6525ec0
JasmineRain <530781348@qq.com>
2020-11-05 20:38:14 +0800

    2020.11.05

--- 30_Hard_串联所有单词的字串.py
+++ 30_Hard_串联所有单词的字串.py
@@ -1,12 +1,13 @@
 from typing import List
+from collections import Counter
 
 
 class Solution:
     def findSubstring_Iterate(self, s: str, words: List[str]) -> List[int]:
-        words_map = {}
-        for word in words:
-            words_map.setdefault(word, 0)
-            words_map[word] += 1
+        words_map = Counter(words)
+        # for word in words:
+        #     words_map.setdefault(word, 0)
+        #     words_map[word] += 1
         ans = []
         length = len(s)
         word_len = len(words[0])
@@ -31,7 +32,33 @@ class Solution:
                 ans.append(i)
         return ans
 
+    def findSubstring_IterateByWord(self, s: str, words: List[str]) -> List[int]:
+        words_map = Counter(words)
+        ans = []
+        length = len(s)
+        word_len = len(words[0])
+        word_num = len(words)
+        for i in range(word_len):
+            cur_words = {}
+            cur_word_num = 0
+            left = right = i
+            while right + word_len <= length:
+                cur_word = s[right: right + word_len]
+                right += word_len
+                cur_words.setdefault(cur_word, 0)
+                cur_words[cur_word] += 1
+                cur_word_num += 1
+                while cur_words[cur_word] > words_map[cur_word]:
+                    drop = s[left: left + word_len]
+                    left += word_len
+
+                    cur_words[drop] -= 1
+                    cur_word_num -= 1
+                if cur_word_num == word_num:
+                    ans.append(left)
+        return ans
+
 
 if __name__ == "__main__":
     S = Solution()
-    print(S.findSubstring_Iterate(s="barfoothefoobarman", words=["foo", "bar"]))
+    print(S.findSubstring_IterateByWord(s="barfoothefoobarman", words=["foo", "bar"]))
