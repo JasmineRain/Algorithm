5a199c183b071fdf4e067cdf03e226287dd9a70d
JasmineRain <530781348@qq.com>
2020-12-14 20:47:41 +0800

    2020.12.14

--- Dynamic Programming/474_Medium_一和零.py
+++ Dynamic Programming/474_Medium_一和零.py
@@ -19,18 +19,16 @@ class Solution:
     #     return dp[-1][-1][-1]
 
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
-        # 定义dp[i][j]为使用i个0，j个1可以拼成的字符串个数
+        # 这里考虑对状态进行压缩，因为状态的第一维k，状态转移时只与前一项有关
+        # 注意遍历顺序，从小至大遍历是不合理的，因为会包含多次选择同一个字符串的情况
         dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
 
         for s in strs:
             n0 = s.count('0')
             n1 = s.count('1')
-            for i in range(m + 1):
-                for j in range(n + 1):
-                    dp[i][j] = dp[i][j]
-
-                    if i >= n0 and j >= n1:
-                        dp[i][j] = max(dp[i][j], dp[i - n0][j - n1] + 1)
+            for i in range(m, n0 - 1, -1):
+                for j in range(n, n1 - 1, -1):
+                    dp[i][j] = max(dp[i][j], dp[i - n0][j - n1] + 1)
 
         # print(dp)
         return dp[-1][-1]
@@ -38,4 +36,4 @@ class Solution:
 
 if __name__ == "__main__":
     S = Solution()
-    print(S.findMaxForm(strs=["10", "0", "1"], m=1, n=1))
+    print(S.findMaxForm(strs = ["10", "0001", "111001", "1", "0"], m = 5, n = 3))
