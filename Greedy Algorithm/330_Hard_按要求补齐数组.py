db1bf8f126f4e2c995d3d43c9b5819f668955798
JasmineRain <530781348@qq.com>
2021-01-04 23:16:01 +0800

    2021.1.4

--- Greedy Algorithm/330_Hard_按要求补齐数组.py
+++ Greedy Algorithm/330_Hard_按要求补齐数组.py
@@ -3,7 +3,18 @@ from typing import List
 
 class Solution:
     def minPatches(self, nums: List[int], n: int) -> int:
-        pass
+        patches, x = 0, 1
+        length, index = len(nums), 0
+
+        while x <= n:
+            if index < length and nums[index] <= x:
+                x += nums[index]
+                index += 1
+            else:
+                x <<= 1
+                patches += 1
+
+        return patches
 
 
 if __name__ == "__main__":
