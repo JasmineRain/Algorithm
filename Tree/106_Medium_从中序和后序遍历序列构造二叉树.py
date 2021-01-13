2314e0a44000c5135d414f16fab2eaec7594c78b
JasmineRain <530781348@qq.com>
2021-01-13 21:12:25 +0800

    2021.1.13

--- Tree/106_Medium_从中序和后序遍历序列构造二叉树.py
+++ Tree/106_Medium_从中序和后序遍历序列构造二叉树.py
@@ -15,23 +15,39 @@ class TreeNode:
 class Solution:
 
     # 递归法
-    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
-
-        if not inorder:
-            return None
-
-        root = TreeNode(postorder[-1])
-        if len(postorder) == 1:
-            return root
-        skip = inorder.index(postorder[-1])
-        root.left = self.buildTree(inorder[:skip], postorder[: skip])
-        root.right = self.buildTree(inorder[skip + 1:], postorder[skip: -1])
-        return root
+    # def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+    #
+    #     if not inorder:
+    #         return None
+    #
+    #     root = TreeNode(postorder[-1])
+    #     if len(postorder) == 1:
+    #         return root
+    #     skip = inorder.index(postorder[-1])
+    #     root.left = self.buildTree(inorder[:skip], postorder[: skip])
+    #     root.right = self.buildTree(inorder[skip + 1:], postorder[skip: -1])
+    #     return root
 
     # 迭代法
-    # def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
-    #     if not preorder:
-    #         return None
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+        if not inorder:
+            return None
+        root = TreeNode(postorder[-1])
+        stack = [root]
+        inorder_idx = len(postorder) - 1
+
+        for i in range(len(postorder) - 2, -1, -1):
+            node = stack[-1]
+            if node.val != inorder[inorder_idx]:
+                node.right = TreeNode(postorder[i])
+                stack.append(node.right)
+            else:
+                while stack and stack[-1].val == inorder[inorder_idx]:
+                    node = stack.pop()
+                    inorder_idx -= 1
+                node.left = TreeNode(postorder[i])
+                stack.append(node.left)
+        return root
 
 
 # if __name__ == "__main__":
